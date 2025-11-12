f463fa41e50eaa61940455752ceaa7b4f46bf723
LakConrad <chris@buyrailings.com>
2025-11-12 15:39:18 -0500

    Create __manifest__.py

---
+++ first_module/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'New App',
+    'summary': 'Small description of your module.',
+    'license': 'OPL-1',
+    'category': 'Category/Subcategory',
+    'author': '', #Use your GitHub Username
+    'website': '', # Link to your Repo
+    'version': '1.0.0',
+    'depends': ['base'], # At minimum depend on 'base'
+    'data': [
+
+    ],
+    'demo': [
+        
+    ],
+    'application': True,
+}
